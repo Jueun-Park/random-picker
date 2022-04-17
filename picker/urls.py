d32350de600630ed959898c8b9866c270bd05f94
Jueun-Park <pjueun08@gmail.com>
2022-04-18 01:30:26 +0900

    post

--- picker/urls.py
+++ picker/urls.py
@@ -3,5 +3,5 @@ from picker import views
 
 urlpatterns = [
     path('', views.index),
-    path('pick/<lines>', views.pick),
+    path('pick/', views.pick),
 ]
--- picker/views.py
+++ picker/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import redirect
 import random
 
 
-def index(request):
-    return HttpResponse('index')
+pick_res = 'ready to pick'
 
-def pick(request, lines):
-    lines_list = lines.split(',')
-    picked_line = random.choice(lines_list)
-    result = f'<h2>{lines}</h2> <h1>{picked_line}</h1>'
-    return HttpResponse(result)
+def picker_template(pick_result='ready to pick'):
+    return f'''
+    <h1>Random Picker</h1>
+    <h2>{pick_result}</h2>
+    <h3>separate by line break</h3>
+    <form action='/pick/' method='POST'>
+        <p><textarea name='lines'></textarea></p>
+        <p><input type='submit'></p>
+    </form>
+    '''
+
+
+def index(request):
+    global pick_res
+    return HttpResponse(picker_template(pick_res))
+
+
+@csrf_exempt
+def pick(request: HttpRequest):
+    global pick_res
+    lines_list = request.POST['lines'].split()
+    pick_res = random.choice(lines_list)
+    return redirect('/')
