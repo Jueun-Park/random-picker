f66c3d08839a7155ca3eb3db4f0bf24948feeacf
Jueun-Park <pjueun08@gmail.com>
2022-05-29 15:13:36 +0900

    separate template

--- picker/urls.py
+++ picker/urls.py
@@ -1,14 +1,16 @@
+from django import views
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import PickerView
+from . import views
 
 
 picker_list = PickerView.as_view({
-    'get': 'index',
     'post': 'pick',
 })
 
 urlpatterns = format_suffix_patterns([
     path('auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('picker/', picker_list, name='picker_list'),
+    path('', views.random_picker_index, name='random_picker_index')
 ])
